d0e5d29ef69b4f2a4f7934987db3a21cd94f7c2c
lilyzhangg1 <lilydini@gmail.com>
2024-03-17 23:12:59 -0400

    feat: create login class

--- main.cpp
+++ main.cpp
@@ -4,8 +4,36 @@
 #include <windows.h>
 using namespace std;
 
+const char* HOST = "localhost";
+const char* USER = "root";
+const char* PW = " ";
+const char* DB = "mydb";
+
+class Login {
+    private:
+        string Id, PW;
+    public:
+        Login(): Id(""), PW("") {}
+
+        void setId(string id) {
+            Id = id; 
+        }
+
+        void setPW (string pw) {
+            PW = pw;
+        }
+
+        string getId() {
+            return Id;
+        }
+
+        string getPW () {
+            return PW;
+        }
+}
+
 int main() {
     
     return 0;
-    
+
 }
